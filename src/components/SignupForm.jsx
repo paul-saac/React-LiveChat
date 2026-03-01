04ffda6231ac0f576482cb52cbc16d1d01eb89a1
paul-saac <johnpaul.saac.legaspi@gmail.com>
2026-03-01 17:47:43 +0800

    login

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -9,17 +9,19 @@ export default function SignupForm() {
     return (
         <div>
             <h2>Sing up Form</h2>
-            <form style={{ display: "flex", flexDirection: "column"}} onsubmit={
+            <form style={{ display: "flex", flexDirection: "column" }} onSubmit={
                 (e) => {
                     e.preventDefault();
                     createUserWithEmailAndPassword(auth, email, password);
                 }
             }>
+                <label>Email:</label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+ 
+                <label>Password:</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-
-
-
-                
+                <button type="submit">Sign Up</button>
             </form>
 
         </div>
